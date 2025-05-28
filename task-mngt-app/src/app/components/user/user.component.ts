d4b9353fdd58363a6e58ec03b22241dac203024e
Shubham <shubham.sharma4390@gmail.com>
2025-05-28 15:28:20 +0530

    implemented custom event using EventEmitter

--- task-mngt-app/src/app/components/user/user.component.ts
+++ task-mngt-app/src/app/components/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -7,12 +7,16 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input() avatar!: string;
-  @Input() name!: string;
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
 
   get imagePath() {
     return 'users/' + this.avatar;
   }
 
-  onSelectUser(): void {}
+  onSelectUser(): void {
+    this.select.emit(this.id);
+  }
 }
