ac6edbf8e193ed6786360fdc8b20d4a902245ba0
Shubham <shubham.sharma4390@gmail.com>
2025-05-29 05:22:05 +0530

    used Type alias and Interface for declaring object in @Input decorator

--- task-mngt-app/src/app/components/user/user.component.ts
+++ task-mngt-app/src/app/components/user/user.component.ts
@@ -1,5 +1,17 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
+// interface User {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// }
+
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// };
+
 @Component({
   selector: 'app-user',
   imports: [],
@@ -7,16 +19,22 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
-  @Output() select = new EventEmitter();
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: {
+    id: string;
+    avatar: string;
+    name: string;
+  };
+  @Output()
+  select = new EventEmitter();
 
   get imagePath() {
-    return 'users/' + this.avatar;
+    return 'users/' + this.user.avatar;
   }
 
   onSelectUser(): void {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
