923397b7686e6bb674a7239d6703b65e309a3d33
Shubham <shubham.sharma4390@gmail.com>
2025-05-29 11:50:44 +0530

    Implemented adding task feature and handled the cancel button and submit button

--- task-mngt-app/src/app/components/tasks/tasks.component.ts
+++ task-mngt-app/src/app/components/tasks/tasks.component.ts
@@ -1,15 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from '../task/task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
+import { type NewTaskData } from '../task/task.model';
 
 @Component({
   selector: 'app-tasks',
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   @Input({ required: true }) name?: string;
-  @Input({ required: true }) userId?: string;
+  @Input({ required: true }) userId!: string;
+  isAddingTask = false;
+
   tasks = [
     {
       id: 't1',
@@ -36,5 +40,28 @@ export class TasksComponent {
 
   onCompleteTask(id: string): void {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  onStartAddTask(): void {
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask(): void {
+    this.isAddingTask = false;
+  }
+
+  onSubmitAddTask(): void {
+    this.isAddingTask = true;
+  }
+
+  onAddTask(taskData: NewTaskData) {
+    this.tasks.unshift({
+      id: new Date().getTime().toString(),
+      userId: this.userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date,
+    });
+    this.isAddingTask = false;
   }
 }
