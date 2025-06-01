b1436609606197ef6ccf27a3ac6decebed9edc05
Shubham <shubham.sharma4390@gmail.com>
2025-06-01 07:57:27 +0530

    reactive forms implementation

--- Forms-app/src/app/auth/login/login.component.ts
+++ Forms-app/src/app/auth/login/login.component.ts
@@ -1,21 +1,21 @@
 import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
+  imports: [ReactiveFormsModule],
   standalone: true,
-  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  onSubmit(formData: NgForm): void {
-    if (formData.form.invalid) {
-      alert('Please fill the details');
-    }
-    const enteredEmail = formData.form.value.email;
-    const enteredPass = formData.form.value.password;
+  form = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
 
-    console.log(enteredEmail, enteredPass);
+  onSubmit(): void {
+    console.log(this.form.value.email);
+    console.log(this.form.value.password);
   }
 }
